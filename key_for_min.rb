b45a63ae03d8723730000726046fb3c8fe9d73ec
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-15 19:36:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,10 +6,12 @@ def key_for_min_value(name_hash)
     return nil;
   end
   lowest = Float::INFINITY;
+  key = "";
   name_hash.each do |item, amount|
     if amount < lowest
       lowest = amount;
-      item;
+      key = item;
     end
   end
+  key;
 end
