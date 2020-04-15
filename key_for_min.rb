bf1fd2db0cdf3e4a8b930cfb3a41a1a08e234e64
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-15 19:37:21 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash == {}
+    return nil;
+  end
+  lowest = Float::INFINITY;
+  key = "";
+  name_hash.each do |item, amount|
+    if amount < lowest
+      lowest = amount;
+      key = item;
+    end
+  end
+  key;
 end
