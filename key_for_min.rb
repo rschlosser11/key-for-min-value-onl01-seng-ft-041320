9ab120d01c52e98c5f04807beb6b717f44d0a125
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-15 19:31:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash == {}
+    return nil;
+  end
+  lowest = Infinity;
+  name_hash.each do |key, value|
+    if value < lowest
+      lowest = value;
+      key;
+    end
+  end
 end
